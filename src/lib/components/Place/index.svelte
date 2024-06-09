90e1661cd9ad6d94c6d35417253f60cdd59ddb7e
Leonardo Luize <leonardo.luize2@gmail.com>
2024-06-09 12:31:22 -0300

    add places

---
+++ src/lib/components/Place/index.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  interface Place {
+    name: string;
+    image: string;
+    likes: number;
+    rating: number;
+  }
+  export let place: Place;
+  let isFilled = false;
+
+  function handleLike() {
+    isFilled = !isFilled;
+  }
+</script>
+
+<div class="w-full md:w-[200px] card hover:shadow-md transition-all">
+  <img
+    src={place.image}
+    alt={place.name}
+    class="w-full h-32 object-cover rounded-t-lg"
+  />
+
+  <div class="p-4">
+    <h2 class="text-xl font-semibold mb-2">{place.name}</h2>
+    <div class="w-full flex justify-between items-center gap-5">
+      <div class="flex items-center gap-2">
+        <span class="material-symbols-rounded"> verified </span>
+        <p class="text-gray-600">{place.rating}</p>
+      </div>
+      <button
+        on:click={handleLike}
+        class="btn variant-ghost-primary py-1 px-3 flex items-center gap-2"
+      >
+        <span
+          class={`material-symbols-rounded text-primary-700 ${
+            isFilled ? 'filled' : ''
+          }`}
+        >
+          thumb_up
+        </span>
+        <p class="text-gray-600">{place.likes}</p>
+      </button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .filled {
+    font-variation-settings: 'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 48;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { api } from '$lib/api';
   import MoodySvg from '$lib/assets/moody.svg';
   import SelectUser from '$lib/components/SelectUser/index.svelte';
+  import Place from '$lib/components/Place/index.svelte';
   import { onMount } from 'svelte';
 
   let selectedUser = 1;
@@ -61,6 +62,80 @@
         'https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=2823&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     },
   ];
+  let places = [
+    {
+      name: 'Café do Ponto',
+      image: 'https://picsum.photos/300/300',
+      rating: 4.5,
+      likes: 10,
+    },
+    {
+      name: 'Pizzaria Bella',
+      image: 'https://picsum.photos/200/300',
+      rating: 4.5,
+      likes: 12,
+    },
+    {
+      name: 'Hamburgueria',
+      image: 'https://picsum.photos/205/330',
+      rating: 4.3,
+      likes: 18,
+    },
+    {
+      name: 'Restaurante de Sushi',
+      image: 'https://picsum.photos/240/340',
+      rating: 4.7,
+      likes: 25,
+    },
+    {
+      name: 'Cafeteria',
+      image: 'https://picsum.photos/250/250',
+      rating: 4.2,
+      likes: 15,
+    },
+    {
+      name: 'Padaria Delícia',
+      image: 'https://picsum.photos/270/270',
+      rating: 4.6,
+      likes: 8,
+    },
+    {
+      name: 'Paraíso do Sorvete',
+      image: 'https://picsum.photos/280/280',
+      rating: 4.8,
+      likes: 20,
+    },
+    {
+      name: 'Café no Parque',
+      image: 'https://picsum.photos/220/220',
+      rating: 4.4,
+      likes: 14,
+    },
+    {
+      name: 'Trattoria Italiana',
+      image: 'https://picsum.photos/230/230',
+      rating: 4.6,
+      likes: 22,
+    },
+    {
+      name: 'Cantina Mexicana',
+      image: 'https://picsum.photos/240/240',
+      rating: 4.3,
+      likes: 17,
+    },
+    {
+      name: 'Churrascaria',
+      image: 'https://picsum.photos/250/250',
+      rating: 4.7,
+      likes: 28,
+    },
+    {
+      name: 'Cozinha Tailandesa',
+      image: 'https://picsum.photos/260/260',
+      rating: 4.5,
+      likes: 19,
+    },
+  ];
   $: currentUser = users.find((x) => x.id === selectedUser);
 
   onMount(() => {
@@ -106,17 +181,21 @@
 </script>
 
 <div>
-  <header class="flex justify-center py-5 shadow-md">
+  <header
+    class="flex justify-center py-5 shadow-md flex-col md:flex-row items-center gap-5 md:gap-0 md:items-start"
+  >
     <div class="flex gap-2 items-center ml-5">
       <img class="h-[30px] pt-1" src={MoodySvg} alt="moody logo" />
       <span class="text-3xl font-thin">/</span>
       <span class="text-2xl font-thin tracking-[0.1em]">Prototipe</span>
     </div>
 
-    <button class="btn ml-auto variant-filled-primary mr-5">Como Usar?</button>
+    <button class="btn ml-auto variant-filled-primary mr-5 w-[90%] md:w-fit"
+      >Como Usar?</button
+    >
   </header>
 
-  <main class="flex w-full items-center flex-col">
+  <main class="flex w-full items-center flex-col px-5 md:px-0">
     <div class="max-w-[1200px] w-full mt-10">
       <h1 class="text-xl">Selecione um usuário:</h1>
       <SelectUser {users} bind:selectedUser />
@@ -127,6 +206,12 @@
         <h2 class="text-xl">
           Lugares selecionados para {currentUser && currentUser.name}:
         </h2>
+
+        <div class="w-full flex flex-wrap items-start gap-10 my-10">
+          {#each places as place}
+            <Place {place} />
+          {/each}
+        </div>
       </div>
     {/if}
   </main>
