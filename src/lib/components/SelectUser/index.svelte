3d718cc7fb18d51a4abed76908c40846c3a07271
Leonardo Luize <leonardo.luize2@gmail.com>
2024-06-09 11:23:39 -0300

    update select user input

--- src/lib/components/SelectUser/index.svelte
+++ src/lib/components/SelectUser/index.svelte
@@ -7,27 +7,8 @@
     image: string;
   }
 
-  let users: User[] = [
-    {
-      id: 1,
-      name: 'User 1',
-      image:
-        'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 2,
-      name: 'User 2',
-      image:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 3,
-      name: 'User 3',
-      image:
-        'https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=2823&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-  ];
-  let selectedUser: number | null = null;
+  export let users: User[]
+  export let selectedUser: number = 0;
 
   onMount(() => {
     // Fetch users data from API or any other source
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
   import MoodySvg from '$lib/assets/moody.svg';
   import SelectUser from '$lib/components/SelectUser/index.svelte';
+
+  let selectedUser = 1;
+  let users = [
+    {
+      id: 1,
+      name: 'User 1',
+      image:
+        'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 2,
+      name: 'User 2',
+      image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 3,
+      name: 'User 3',
+      image:
+        'https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=2823&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ];
+  $: currentUser = users.find((x) => x.id === selectedUser);
 </script>
 
 <div>
@@ -16,8 +39,16 @@
 
   <main class="flex w-full items-center flex-col">
     <div class="max-w-[1200px] w-full mt-10">
-      <h1>Selecione um usuário</h1>
-      <SelectUser />
+      <h1 class="text-xl">Selecione um usuário:</h1>
+      <SelectUser {users} bind:selectedUser />
     </div>
+
+    {#if selectedUser != 0}
+      <div class="max-w-[1200px] w-full mt-10">
+        <h2 class="text-xl">
+          Lugares selecionados para {currentUser && currentUser.name}:
+        </h2>
+      </div>
+    {/if}
   </main>
 </div>
